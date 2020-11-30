afcc352dc03fed00f209c319e86cf9a66d7945f9
13959673160 <74644775+13959673160@users.noreply.github.com>
2020-11-30 21:37:42 +0800

    Update twice.java

--- Secondly/src/twice.java
+++ Secondly/src/twice.java
@@ -3,9 +3,14 @@ public class twice {
 
 	public static void main(String[] args) {
 		System.out.print("ÄãºÃ£¬Ôø¿¡ÖÇ");
-		System.out.print("z");
-		System.out.print("j");
-		System.out.print("z");
+		[branch "master"] 
+	remote = origin 
+	merge = refs/heads/master
+[remote "origin"] 
+	url = https://github.com/huangbangyi2019/git_project.git 
+	fetch = +refs/heads/*:refs/remotes/origin/* 
+	push = refs/heads/master:refs/heads/master
+
 		
 	}
 
